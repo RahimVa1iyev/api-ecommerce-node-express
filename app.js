185e61622f1e07f3670ab166fed101e7d8cdae38
rahimvaliyev <rahimvaliyev99@gmail.com>
2024-01-03 18:24:42 +0400

    setup-express

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('E-Commerce API');
+const express = require('express')
+const app = express()
+
+
+
+const port = 3000
+
+const start = async () => {
+    try {
+     app.listen(port, console.log(`Server is listening ${port}`))
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
