d768f9a0d814dc1ec19d4840f426fb2183adee22
rahimvaliyev <rahimvaliyev99@gmail.com>
2024-01-04 12:22:46 +0400

    setup-register-controller-hashed-password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,16 +1,27 @@
 const User = require('../models/User')
+const { BadRequestError } = require('../errors')
+const {StatusCodes} = require('http-status-codes')
 
 
-const login = async (req,res) =>{
+const login = async (req, res) => {
     res.send("login fetch")
 }
 
-const register = async (req,res) =>{
-    res.send("register fetch")
+const register = async (req, res) => {
+    const { name, email, password } = req.body
+
+    const isFirstAccount = (await User.countDocuments({})) === 0
+    const role = isFirstAccount ? 'admin' : 'user'
+
+
+
+    const user = await User.create({name,email,password,role})
+
+    res.status(StatusCodes).json({user})
 }
 
-const logout = async (req,res) =>{
+const logout = async (req, res) => {
     res.send("logout fetch")
 }
 
-module.exports = {login,register,logout}
+module.exports = { login, register, logout }
--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
+
 
 const UserSchema = new mongoose.Schema({
     name:{
@@ -10,13 +12,14 @@ const UserSchema = new mongoose.Schema({
     },
     email:{
         type : String,
+        unique:[true, 'Email already exist'],
         required :[true,'Email is provided'],
         validate : {
             validator : validator.isEmail,
             message : 'Please provide valid email'
         }
     },
-    name:{
+    password:{
         type : String,
         required :[true,'Password is provided'],
         minlength:6
@@ -26,5 +29,15 @@ const UserSchema = new mongoose.Schema({
         enum :['admin','user']
     }
 })
+
+UserSchema.pre('save',async function(){
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password ,salt)
+})
+
+UserSchema.methods.comparePassword = async function(canditatePassword){
+    const isMatch = await bcrypt.compare(canditatePassword,this.password)
+    return isMatch
+}
 
 module.exports = mongoose.model("User",UserSchema)
